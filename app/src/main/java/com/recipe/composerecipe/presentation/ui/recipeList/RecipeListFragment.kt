2ce6d95da61e74eb942b737d1d5ff54b6049bb97
Umar Saidu Auna <umarmanofpeace@gmail.com>
2022-01-16 10:07:14 +0100

    feat: 🎸 SearchView Toolbar

--- app/src/main/java/com/recipe/composerecipe/presentation/ui/recipeList/RecipeListFragment.kt
+++ app/src/main/java/com/recipe/composerecipe/presentation/ui/recipeList/RecipeListFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
@@ -23,6 +34,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class RecipeListFragment : Fragment() {
     private val viewModel: RecipeListViewModel by viewModels()
 
+    @ExperimentalComposeUiApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -33,15 +45,49 @@ class RecipeListFragment : Fragment() {
                 // SurfaceViewRecipeList()
                 val recipes = viewModel.recipes.value
                 val query = viewModel.query.value
+                val keyboardController = LocalSoftwareKeyboardController.current
 
                 Column {
-                    TextField(
-                        value = query,
-                        onValueChange = { newValue ->
-                            viewModel.onQueryChanged(newValue)
-                        },
-                    )
-                    Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        color = MaterialTheme.colors.primary,
+                        elevation = 8.dp
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            TextField(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.9f)
+                                    .padding(8.dp),
+                                value = query,
+                                onValueChange = { newValue ->
+                                    viewModel.onQueryChanged(newValue)
+                                },
+                                label = { Text(text = "Search") },
+                                keyboardOptions = KeyboardOptions(
+                                    keyboardType = KeyboardType.Text,
+                                    imeAction = ImeAction.Search
+                                ),
+                                leadingIcon = {
+                                    Icon(Icons.Filled.Search, "Search")
+                                },
+                                keyboardActions = KeyboardActions(
+                                    onSearch = {
+                                        viewModel.newSearch(query)
+                                        keyboardController?.hide()
+                                    }
+                                ),
+                                textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                colors = TextFieldDefaults.textFieldColors(
+                                    backgroundColor = MaterialTheme.colors.surface
+                                )
+                            )
+                        }
+                    }
+                    // Spacer(modifier = Modifier.padding(10.dp))
                     LazyColumn {
                         itemsIndexed(
                             items = recipes
