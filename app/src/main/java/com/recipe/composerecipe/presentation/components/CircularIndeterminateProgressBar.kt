75a828993fb158c43d58c0dc5b28d7553274ccb3
Umar Saidu Auna <umarmanofpeace@gmail.com>
2022-01-16 13:48:32 +0100

    feat: 🎸 Constraint Layout

--- app/src/main/java/com/recipe/composerecipe/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/recipe/composerecipe/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,28 +1,32 @@
 package com.recipe.composerecipe.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun CircularIndeterminateProgressBar(
     isDisplayed: Boolean
 ) {
     if (isDisplayed) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(50.dp),
-            horizontalArrangement = Arrangement.Center
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize(),
+
         ) {
+            val progressBar = createRef()
+            val topGuideline = createGuidelineFromTop(0.3f)
             CircularProgressIndicator(
-                color = MaterialTheme.colors.primary
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier
+                    .constrainAs(progressBar) {
+                        top.linkTo(topGuideline)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+
             )
         }
     }
