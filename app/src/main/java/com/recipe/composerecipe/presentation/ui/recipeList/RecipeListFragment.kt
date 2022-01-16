e1b0ae63e3262509402dfb5ef62ca5b469c586b9
Umar Saidu Auna <umarmanofpeace@gmail.com>
2022-01-16 08:53:30 +0100

    feat: 🎸 Capturing User Input

--- app/src/main/java/com/recipe/composerecipe/presentation/ui/recipeList/RecipeListFragment.kt
+++ app/src/main/java/com/recipe/composerecipe/presentation/ui/recipeList/RecipeListFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.recipe.composerecipe.presentation.components.RecipeCard
@@ -27,11 +32,22 @@ class RecipeListFragment : Fragment() {
             setContent {
                 // SurfaceViewRecipeList()
                 val recipes = viewModel.recipes.value
-                LazyColumn {
-                    itemsIndexed(
-                        items = recipes
-                    ) { index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = {})
+                val query = viewModel.query.value
+
+                Column {
+                    TextField(
+                        value = query,
+                        onValueChange = { newValue ->
+                            viewModel.onQueryChanged(newValue)
+                        },
+                    )
+                    Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
+                    LazyColumn {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = {})
+                        }
                     }
                 }
             }
